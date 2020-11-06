033c75b0ca98f30abdb2d46039c496aec62631f9
Ola Opheim <olaopheim@LT-olaopheim.local>
2020-11-06 11:06:01 +0100

    Built example as sirius-lecture (without tools)

--- tdt4250.cb.model/src/tdt4250/cb/impl/CbPackageImpl.java
+++ tdt4250.cb.model/src/tdt4250/cb/impl/CbPackageImpl.java
@@ -618,7 +618,7 @@ public class CbPackageImpl extends EPackageImpl implements CbPackage {
 		initEAttribute(getTrip_Duration(), ecorePackage.getEInt(), "duration", null, 0, 1, Trip.class, !IS_TRANSIENT, !IS_VOLATILE, IS_CHANGEABLE, !IS_UNSETTABLE, !IS_ID, IS_UNIQUE, IS_DERIVED, IS_ORDERED);
 
 		initEClass(bikeEClass, Bike.class, "Bike", !IS_ABSTRACT, !IS_INTERFACE, IS_GENERATED_INSTANCE_CLASS);
-		initEAttribute(getBike_Id(), ecorePackage.getEInt(), "id", null, 0, 1, Bike.class, !IS_TRANSIENT, !IS_VOLATILE, IS_CHANGEABLE, !IS_UNSETTABLE, IS_ID, IS_UNIQUE, !IS_DERIVED, IS_ORDERED);
+		initEAttribute(getBike_Id(), ecorePackage.getEInt(), "id", null, 1, 1, Bike.class, !IS_TRANSIENT, !IS_VOLATILE, IS_CHANGEABLE, !IS_UNSETTABLE, IS_ID, IS_UNIQUE, !IS_DERIVED, IS_ORDERED);
 		initEAttribute(getBike_Name(), ecorePackage.getEString(), "name", null, 0, 1, Bike.class, !IS_TRANSIENT, !IS_VOLATILE, IS_CHANGEABLE, !IS_UNSETTABLE, !IS_ID, IS_UNIQUE, !IS_DERIVED, IS_ORDERED);
 		initEReference(getBike_CurrentStation(), this.getStation(), this.getStation_AvailableBikes(), "currentStation", null, 0, 1, Bike.class, !IS_TRANSIENT, !IS_VOLATILE, IS_CHANGEABLE, !IS_COMPOSITE, IS_RESOLVE_PROXIES, !IS_UNSETTABLE, IS_UNIQUE, !IS_DERIVED, IS_ORDERED);
 		initEAttribute(getBike_LastServiceTime(), ecorePackage.getEDate(), "lastServiceTime", null, 0, 1, Bike.class, !IS_TRANSIENT, !IS_VOLATILE, IS_CHANGEABLE, !IS_UNSETTABLE, !IS_ID, IS_UNIQUE, IS_DERIVED, IS_ORDERED);
--- tdt4250.cb.model/src/tdt4250/cb/util/ImportHelper.java
+++ tdt4250.cb.model/src/tdt4250/cb/util/ImportHelper.java
@@ -77,6 +77,7 @@ public class ImportHelper {
 		for (int i = 0; i < 100; i++) {
 			
 			Bike bike = factory.createBike();
+			// FIXME: bike.setId(i);
 			bike.setName(names.get(i%names.size()));
 
 			try {
