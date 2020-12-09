f81f3f4c0a38a127b39161e60b6cf1cc47e03cd3
Håvard <havard.farestveit@gmail.com>
2020-12-09 16:57:36 +0100

    Updated Odesign

--- tdt4250.cb.model/tdt4250.cb.design/src/tdt4250/cb/design/Services.java
+++ tdt4250.cb.model/tdt4250.cb.design/src/tdt4250/cb/design/Services.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.Date;
 import java.util.List;
+import java.util.Random;
 import java.util.stream.Collectors;
 
 import org.eclipse.emf.ecore.EObject;
@@ -18,22 +19,6 @@ import tdt4250.cb.Trip;
  * The services class used by VSM.
  */
 public class Services {
-    
-    /**
-    * See http://help.eclipse.org/neon/index.jsp?topic=%2Forg.eclipse.sirius.doc%2Fdoc%2Findex.html&cp=24 for documentation on how to write service methods.
-    */
-    public String myService(EObject self) {
-       // TODO Auto-generated code
-      return self.getClass().toString();
-    }
-    
-    public String getBikeName(Bike bike) {
-    	return bike.getName();
-    }
-    
-    public String getReports(Bike bike){
-    	return bike.getServiceReports().toString();
-    }
     
     public float getAvgDuration(Bike bike){
     	City city = (City) bike.eContainer();
@@ -95,27 +80,49 @@ public class Services {
         return new Date();
      }
          
-     public List<Bike> getBikesWithNoService(City city) {
-     	return city.getBikes().stream().filter(a -> a.getServiceReports().isEmpty()).collect(Collectors.toList());
+     public List<Bike> getBikesWithNoService(City city) {     
+    	 List<Bike> bikes = city.getBikes();
+    	 for (Bike bike : bikes) {
+    		 if (!bike.getServiceReports().isEmpty()) {
+    	            bikes.remove(bike);
+    	        }
+		}
+	     return bikes;
      }
      
      public List<Bike> getBikesForService(City city) {
-     	return city.getBikes();
-     }
+    	 List<Bike> bikes = city.getBikes();
+// 	    for(Bike bike: bikes) {
+// 	    	if(!bike.getServiceReports().isEmpty()) {
+//	            if(this.bikeNeedsNewService(bike)) {
+//    	        	bikes.remove(bike);
+//	            }
+//	        }
+// 	    }
+// 	    bikes.sort(Comparator.comparing(Bike::getLastServiceTime));
+ 	    return bikes;     }
      
      public List<Bike> getServicedBikes(City city) {
-     	List<Bike> bikes = city.getBikes().stream().filter(a -> !a.getServiceReports().isEmpty() && !bikeNeedsNewService(a)).collect(Collectors.toList());
-     	bikes.sort(Comparator.comparing(Bike::getLastServiceTime));
-     	return bikes;
-     }
+    	    List<Bike> bikes = city.getBikes();
+//    	    for(Bike bike: bikes) {
+//    	        if(!bike.getServiceReports().isEmpty()) {
+//    	            if(!this.bikeNeedsNewService(bike)) {
+//        	        	bikes.remove(bike);
+//    	            }
+//    	        }
+//    	    }
+//    	    bikes.sort(Comparator.comparing(Bike::getLastServiceTime));
+    	    return bikes;
+    	}
      
      private boolean bikeNeedsNewService(Bike bike) {
      	//int thresholdDays = 180;
  		//TODO: add after testing
      	//Date thresholdDate = new Date(System.currentTimeMillis() - thresholdDays * 24 * 60 * 60 * 1000);
      	Date thresholdDate = new Date();
+     	Random rand = new Random();
      	
-     	return thresholdDate.after(bike.getLastServiceTime());
+     	return rand.nextInt(30) > 15;
      }
     
     
